6dbd3f48173c2fa49be4d7b3918fbbaa7eef41db
matyX6 <matyX6@gmail.com>
2019-06-29 15:01:55 +0200

    RPG3 - basic raycast

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Ray lastRay;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+        transform.GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
