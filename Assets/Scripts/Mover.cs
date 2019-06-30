47e45a62da129b6d57d005c66b368da4c6d5144d
matyX6 <matyX6@gmail.com>
2019-06-30 11:01:15 +0200

    RPG6 - movement animation on player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -11,6 +11,8 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -24,5 +26,13 @@ public class Mover : MonoBehaviour
         {
             transform.GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = transform.GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
