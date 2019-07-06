43f783dce53cf3525a1ae9d3929670316e0c76d8
matyX6 <matyX6@gmail.com>
2019-07-06 13:37:14 +0200

    RPG13 - decoupling dependency cycle

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,4 +1,5 @@
 ﻿using RPG.Combat;
+using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -20,6 +21,7 @@ namespace RPG.Movement
 
         public void StartMoveAction(Vector3 destination)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             GetComponent<Fighter>().Cancel();
             MoveTo(destination);
         }
